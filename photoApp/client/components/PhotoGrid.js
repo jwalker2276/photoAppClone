e9d6717902d0ffcf675909175cb6d4f1a4b8fedc
Jordan Walker <jwalker2276@gmail.com>
2018-05-17 18:06:54 -0500

    Added photo component

--- photoApp/client/components/PhotoGrid.js
+++ photoApp/client/components/PhotoGrid.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Photo from "./Photo.js";
 
 const PhotoGrid = React.createClass({
   render() {
-    return <div className="photo-grid">I'm the photo grid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, index) => (
+          <Photo {...this.props} key={index} post={post} index={index} />
+        ))}
+      </div>
+    );
   }
 });
 
