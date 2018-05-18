f5602268f72d74c96c80c40c4d35e3a00a3875bc
Jordan Walker <jwalker2276@gmail.com>
2018-05-17 20:03:05 -0500

    Added comments component and update single.js logic.

--- photoApp/client/components/Single.js
+++ photoApp/client/components/Single.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Photo from "./Photo";
+import Comments from "./Comments";
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">I'm the single-photo</div>;
+    const index = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[index];
+
+    return (
+      <div className="single-photo">
+        <Photo index={index} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 });
 
