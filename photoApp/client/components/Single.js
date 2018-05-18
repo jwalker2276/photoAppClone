a14681b7e4de33b1113677541bf234a44b22c870
Jordan Walker <jwalker2276@gmail.com>
2018-05-17 21:23:51 -0500

    Added new comments and remove comments logic.

--- photoApp/client/components/Single.js
+++ photoApp/client/components/Single.js
@@ -8,11 +8,12 @@ const Single = React.createClass({
       post => post.code === this.props.params.postId
     );
     const post = this.props.posts[index];
+    const postComments = this.props.comments[this.props.params.postId] || [];
 
     return (
       <div className="single-photo">
         <Photo index={index} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     );
   }
--- photoApp/client/reducers/comments.js
+++ photoApp/client/reducers/comments.js
@@ -1,5 +1,31 @@
+const postComments = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_COMMENT":
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case "REMOVE_COMMENT":
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ];
+    default:
+      return state;
+  }
+  return state;
+};
+
 const comments = (state = [], action) => {
-  console.log(state, action);
+  if (typeof action.postId !== "undefined") {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 };
 
